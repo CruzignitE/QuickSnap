fd56af99f4d47aea1842414d076aa5cfd3ae0fe4
asadiqbal93 <asadwalker@hotmail.com>
2018-03-11 19:39:59 +0800

    add game timer logic to Snap and have the game started when the spacebar is pressed

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -37,6 +37,7 @@ namespace CardGames.GameLogic
 		public Snap ()
 		{
 			_deck = new Deck ();
+			_gameTimer = SwinGame.CreateTimer ();
 		}
 
 		/// <summary>
@@ -91,7 +92,8 @@ namespace CardGames.GameLogic
 				_started = true;
 				_deck.Shuffle ();		// Return the cards and shuffle
 
-				FlipNextCard ();		// Flip the first card...
+				FlipNextCard ();        // Flip the first card...
+				_gameTimer.Start ();    // Add line of code to start the game timer
 			}
 		}
 			
@@ -112,6 +114,10 @@ namespace CardGames.GameLogic
 		public void Update()
 		{
 			//TODO: implement update to automatically slip cards!
+			if (_gameTimer.Ticks > _flipTime) {
+				_gameTimer.Reset ();
+				FlipNextCard ();
+			}
 		}
 
 		/// <summary>
@@ -145,6 +151,7 @@ namespace CardGames.GameLogic
 
 				// stop the game...
 	 			_started = false;
+				_gameTimer.Stop (); //Game timer stops once the player hits the cards
 		}
 	
 		#region Snap Game Unit Tests
--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -24,7 +24,7 @@ namespace CardGames
 
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.Start (); // Press Space bar to start the game
 			}
 
 			if (myGame.IsStarted) 
