01f3270e7101893c1a8d181e31b558f6c911557a
min098 <100073905@students.swinburne.edu.my>
2018-03-14 12:36:25 +0800

    Changed SnapGame.cs on the font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -57,7 +57,7 @@ namespace CardGames
 			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
 
 			// Change the font for the score
-			SwinGame.LoadFontNamed ("GameFont", "Chuckfive.otf", 24);
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
@@ -65,7 +65,7 @@ namespace CardGames
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 50);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
